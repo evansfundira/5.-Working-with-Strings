8ec6dda7c3b312880eba304c702c01a05c7b267b
evansfundira <evansfundira@gmail.com>
2023-12-05 08:58:27 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5.Worning_with_Strings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = "John";
+            string lastName = "Thomas";
+
+            //functions with strings
+            Console.WriteLine(firstName + " is " + firstName.Length + " letters in length");
+
+
+            //freeze
+            Console.Read();
+
+
+        }
+    }
+}
